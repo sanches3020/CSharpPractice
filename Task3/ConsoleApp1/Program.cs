f4f9fdb389ae564a2356541ef4cea51d42978262
sanches3020 <evmenovasofia7@gmail.com>
2025-03-20 09:26:07 +0300

    Update Task3
    Update CosoleApp1

--- Task3/ConsoleApp1/Program.cs
+++ Task3/ConsoleApp1/Program.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 
-using System.Runtime.CompilerServices;
-
-class A 
+class A
 {
     private int a;
     private int b;
 
-    public A(int aValue,int bValue)
+    public A(int aValue, int bValue)
     {
         a = aValue;
         b = bValue;
     }
+
     public double Calculate()
     {
         if (a == 0)
@@ -20,8 +19,40 @@ class A
         }
         return (3 * b - 2) / (4.0 * a * a);
     }
+
     public (int, int) Cube()
     {
         return (a * a * a, b * b * b);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение для a:");
+        int aValue = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите значение для b:");
+        int bValue = int.Parse(Console.ReadLine());
+
+        A obj = new A(aValue, bValue);
+
+        Console.WriteLine($"Значение a: {aValue}");
+        Console.WriteLine($"Значение b: {bValue}");
+
+        try
+        {
+            double expressionValue = obj.Calculate();
+            Console.WriteLine($"Значение выражения: {expressionValue}");
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        var (aCubed, bCubed) = obj.Cube();
+        Console.WriteLine($"a в кубе: {aCubed}");
+        Console.WriteLine($"b в кубе: {bCubed}");
     }
 }
