645a99145aac48437534084e245b30921a742c88
sanches3020 <evmenovasofia7@gmail.com>
2025-03-20 09:32:04 +0300

    Add ConsoleApp2
    Add class Person

---
+++ Task3/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
