f0cc5bbfe6ffab9fafb47f14518424dab363be3f
sanches3020 <evmenovasofia7@gmail.com>
2025-03-20 09:00:11 +0300

    Update ConsoleApp1
    Add Class1, method Calculate and  Cube

--- Task3/ConsoleApp1/Program.cs
+++ Task3/ConsoleApp1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+using System.Runtime.CompilerServices;
+
+class A 
+{
+    private int a;
+    private int b;
+
+    public A(int aValue,int bValue)
+    {
+        a = aValue;
+        b = bValue;
+    }
+    public double Calculate()
+    {
+        if (a == 0)
+        {
+            throw new DivideByZeroException("Значение a не может быть равно 0.");
+        }
+        return (3 * b - 2) / (4.0 * a * a);
+    }
+    public (int, int) Cube()
+    {
+        return (a * a * a, b * b * b);
+    }
+}
