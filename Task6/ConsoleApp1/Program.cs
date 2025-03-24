5696e8f78ea64d8e9906b1ee24753b2001ae7952
sanches3020 <evmenovasofia7@gmail.com>
2025-03-24 10:59:44 +0300

    Add ConsoleApp1 in Task6

---
+++ Task6/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public delegate void LightControl(bool state); 
+
+public class RoomLight
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Свет в комнате включен.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Свет в комнате выключен.");
+    }
+}
+
+public class StreetLight
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Уличный свет включен.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Уличный свет выключен.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        RoomLight roomLight = new RoomLight();
+        StreetLight streetLight = new StreetLight();
+
+        LightControl roomLightControl = (state) => {
+            if (state)
+                roomLight.TurnOn();
+            else
+                roomLight.TurnOff();
+        };
+
+        LightControl streetLightControl = (state) => {
+            if (state)
+                streetLight.TurnOn();
+            else
+                streetLight.TurnOff();
+        };
+
+        roomLightControl(true); 
+        streetLightControl(true); 
+
+        roomLightControl(false); 
+        streetLightControl(false); 
+    }
+}
