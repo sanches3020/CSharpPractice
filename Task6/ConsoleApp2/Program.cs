24e0efa45f6fcc0057c5ab913f2c88f14e3d176d
sanches3020 <evmenovasofia7@gmail.com>
2025-03-24 11:00:25 +0300

    Add ConsoleApp2

---
+++ Task6/ConsoleApp2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public delegate double OrderHandler(double amount);
+
+public class OrderProcessor
+{
+    public void HandleOrder(double amount, OrderHandler handler)
+    {
+        double result = handler(amount);
+        Console.WriteLine($"Обработанная сумма: {result}");
+    }
+}
+
+class Program
+{
+    public static double ApplyDiscount(double amount)
+    {
+        double discount = 0.1;
+        return amount - (amount * discount);
+    }
+
+    public static double CalculateTax(double amount)
+    {
+        double taxRate = 0.15;
+        return amount + (amount * taxRate);
+    }
+
+    static void Main()
+    {
+        OrderProcessor processor = new OrderProcessor();
+        double orderAmount = 100.0;
+
+        Console.WriteLine("Применение скидки: ");
+        processor.HandleOrder(orderAmount, ApplyDiscount);
+
+        Console.WriteLine("Расчет налога: ");
+        processor.HandleOrder(orderAmount, CalculateTax);
+    }
+}
