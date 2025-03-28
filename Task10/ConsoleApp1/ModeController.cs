a9e777d0264e7601e0264672df1c72db99f52b6c
sanches3020 <evmenovasofia7@gmail.com>
2025-03-28 11:31:27 +0300

    Add ConsoleApp1 in Task10

---
+++ Task10/ConsoleApp1/ModeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class ModeController
+    {
+   
+        private static ModeController _instance;
+
+        private string _currentMode;
+
+        private ModeController()
+        {
+            _currentMode = "обычный";
+        }
+
+        public static ModeController GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new ModeController();
+            }
+            return _instance;
+        }
+
+        public void SetMode(string mode)
+        {
+            _currentMode = mode;
+        }
+
+        public string GetMode()
+        {
+            return _currentMode;
+        }
+    }
+}
---
+++ Task10/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ModeController modeController = ModeController.GetInstance();
+
+        modeController.SetMode("отладочный");
+        Console.WriteLine($"Текущий режим: {modeController.GetMode()}");
+
+        modeController.SetMode("обычный");
+        Console.WriteLine($"Текущий режим: {modeController.GetMode()}");
+
+        ModeController anotherModeController = ModeController.GetInstance();
+        Console.WriteLine($"Проверка экземпляра: {anotherModeController.GetMode()}");
+    }
+}
