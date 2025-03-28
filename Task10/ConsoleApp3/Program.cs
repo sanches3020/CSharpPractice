77810791999667844bd8eaaa1673c9b297954b5c
sanches3020 <evmenovasofia7@gmail.com>
2025-03-28 11:32:33 +0300

    Add ConsoleApp3

---
+++ Task10/ConsoleApp3/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace RaceGameApp
+{
+    public class RaceGame
+    {
+        private List<IPlayerObserver> _observers = new List<IPlayerObserver>();
+        private string _raceStatus;
+
+        public void Attach(IPlayerObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IPlayerObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(_raceStatus);
+            }
+        }
+
+        public void StartRace()
+        {
+            _raceStatus = "Гонка началась!";
+            Notify();
+        }
+
+        public void UpdateRaceStatus(string status)
+        {
+            _raceStatus = status;
+            Notify();
+        }
+
+        public void EndRace()
+        {
+            _raceStatus = "Гонка закончилась!";
+            Notify();
+        }
+    }
+    public interface IPlayerObserver
+    {
+        void Update(string status);
+    }
+
+    public class Spectator : IPlayerObserver
+    {
+        public void Update(string status)
+        {
+            Console.WriteLine($"Зритель: {status}");
+        }
+    }
+
+    public class RaceCommentator : IPlayerObserver
+    {
+        public void Update(string status)
+        {
+            Console.WriteLine($"Комментатор: {status}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RaceGame raceGame = new RaceGame();
+            Spectator spectator = new Spectator();
+            RaceCommentator commentator = new RaceCommentator();
+
+            raceGame.Attach(spectator);
+            raceGame.Attach(commentator);
+
+            raceGame.StartRace();
+
+            raceGame.UpdateRaceStatus("Игрок 1 лидирует!");
+            raceGame.UpdateRaceStatus("Игрок 2 догоняет!");
+            raceGame.EndRace();
+        }
+    }
+}
