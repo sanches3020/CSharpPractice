0e662e5653e056b2a5f57c79dc70c85fe4106e0b
sanches3020 <evmenovasofia7@gmail.com>
2025-03-29 10:01:36 +0300

    Add VIPSubscription in ConsoleApp1

---
+++ Task10p2/ConsoleApp1/VIPSubscription.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    public class VIPSubscription : ISubscription
+    {
+        public string GetBenefits()
+        {
+            return "VIP подписка: доступ ко всем функциям.";
+        }
+    }
+}
