00200e5a7fa4024c959bc76e0a67a623f8d50c76
sanches3020 <evmenovasofia7@gmail.com>
2025-04-09 22:48:26 +0300

    Add Task20

---
+++ Task20/MiniBlog/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Заголовок обязателен.")]
+        [StringLength(100, ErrorMessage = "Заголовок не может быть длиннее 100 символов.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Содержание обязательно.")]
+        [MinLength(10, ErrorMessage = "Содержание должно содержать не менее 10 символов.")]
+        public string Content { get; set; }
+
+        [Display(Name = "Дата публикации")]
+        public DateTime DatePosted { get; set; }
+    }
+}
---
+++ Task20/MiniBlog/ViewModels/PostViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MiniBlog.ViewModels
+{
+    public class PostViewModel
+    {
+        [Required(ErrorMessage = "Заголовок обязателен.")]
+        [StringLength(100, ErrorMessage = "Заголовок не может превышать 100 символов.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Текст обязателен.")]
+        [MinLength(10, ErrorMessage = "Текст должен содержать минимум 10 символов.")]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "Автор обязателен.")]
+        [StringLength(50, ErrorMessage = "Имя автора не может превышать 50 символов.")]
+        public string Author { get; set; }
+    }
+}
